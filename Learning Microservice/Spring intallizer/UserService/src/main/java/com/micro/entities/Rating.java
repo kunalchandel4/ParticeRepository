6d598f65ee85f87e98d597e67df19559d80df690
Kunal Chandel <kunalchandel266@gmail.com>
2023-09-06 10:50:34 +0530

    feign client implementing

--- Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/entities/Rating.java
+++ Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/entities/Rating.java
@@ -10,5 +10,7 @@ public class Rating {
 	private String hotelId;
 	private Integer rating;
 	private String feedback;
+	
+	private Hotel hotel;
 
 }
--- Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/service/UserServiceImpl.java
+++ Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.micro.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.micro.entities.Hotel;
+import com.micro.entities.Rating;
 import com.micro.entities.User;
 import com.micro.exception.UserException;
+import com.micro.externalService.HotelService;
 import com.micro.repo.UserRepo;
 
 @Service
@@ -14,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepo uR;
+	@Autowired
+	private RestTemplate restTemp;
+
+	@Autowired
+	private HotelService hotelService;
 
 	@Override
 	public User saveUser(User object) {
@@ -32,7 +43,25 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUser(String id) throws UserException {
 		// TODO Auto-generated method stub
-		return uR.findById(id).orElseThrow(()-> new UserException("Not found " + id));
+		User theObject = uR.findById(id).orElseThrow(() -> new UserException("Not found " + id));
+		Rating[] ratingArray = restTemp.getForObject("http://RATING-SERVICE/ratings/user/" + theObject.getUserId(),
+				Rating[].class);
+
+		List<Rating> ratingOfUser = Arrays.stream(ratingArray).toList();
+
+		ratingOfUser.stream().map(rating -> {
+			// http://localhost:9002/hotels/2550c591-7be2-4b1d-8b83-70668a821c11
+//			  Hotel hotel =  restTemp.getForObject("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = hotelService.getHotel(rating.getHotelId());
+			// set hotel to the rating
+			rating.setHotel(hotel);
+			// return rating
+			return rating;
+		}).collect(Collectors.toList());
+
+		theObject.setRating(ratingOfUser);
+
+		return theObject;
 	}
 
 }
