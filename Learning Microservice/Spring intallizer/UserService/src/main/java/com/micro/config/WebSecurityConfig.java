ac1bd4f79410c55ebae288bbc7d2689cd948bd60
Kunal Chandel <kunalchandel266@gmail.com>
2023-09-27 12:08:36 +0530

    +1

--- Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/config/WebSecurityConfig.java
+++ Learning Microservice/Spring intallizer/UserService/src/main/java/com/micro/config/WebSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +28,23 @@ public class WebSecurityConfig {
 		return httpSecurity.build();
 
 	}
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(clientRegistration());
+    }
+
+    private ClientRegistration clientRegistration() {
+        return ClientRegistration.withRegistrationId("okta")
+            .clientId("0oabfxd4tuFZR8SkJ5d7")
+            .clientSecret("g4iIOUTdKPqEJXGlRnR3tQ6w2amBQDCet0bEFUgT-K5Hi0Sj03_BtyRz7KdjWOnc")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+            .scope("internal")
+            .authorizationUri("https://dev-06833922.okta.com/oauth2/default/v1/authorize")
+            .tokenUri("https://dev-06833922.okta.com/oauth2/default/v1/token")
+            .userInfoUri("https://dev-06833922.okta.com/oauth2/default/v1/userinfo")
+            .build();
+    }
 
 }
